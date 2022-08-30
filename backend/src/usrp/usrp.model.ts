51c83dab53d8485a16c4858493de9ea43abce679
asm1th <asm1th@yandex.ru>
2022-08-30 11:31:22 +0300

    Revert "Added fields to the user table"
    This reverts commit f7fa0e8abbc579605e8b63f7820e06994a859f2c.

--- backend/src/usrp/usrp.model.ts
+++ backend/src/usrp/usrp.model.ts
@@ -16,29 +16,29 @@ interface UsrpAttrs{
 
 @Table({tableName:'ztin_suz_kp_usrp', createdAt: false, updatedAt: false})
 export class Usrp extends Model<Usrp, UsrpAttrs> {
-  @ApiProperty({description: 'Ключ расченки в шаблоне КП', example: 'string(32)'})
+  @ApiProperty({description: 'Ключ расченки в шаблоне КП', example: 'varchar(32)'})
   @ForeignKey( () => Unit)
   @Column({type: DataType.STRING(32), primaryKey: true})
   kp_unit_guid: string;
-  @ApiProperty({description: 'Ключ для расценки контрагента в шаблоне КП', example: 'string(32)'})
+  @ApiProperty({description: 'Ключ для расценки контрагента в шаблоне КП', example: 'varchar(32)'})
   @Column({type: DataType.STRING(32), primaryKey: true})
   link_id: string;
-  @ApiProperty({description: 'Стоимость расценки', example: 'number'})
+  @ApiProperty({description: 'Стоимость расценки', example: 'decimal(17,2)'})
   @Column({type: DataType.DECIMAL(17,2), primaryKey: true})
   prices_user: DecimalDataType;
-  @ApiProperty({description: 'Единица измерения', example: 'string(3)'})
+  @ApiProperty({description: 'Единица измерения', example: 'varchar(3)'})
   @Column({type: DataType.STRING(3)})
   usl_quan_unit: string;
-  @ApiProperty({description: 'Количество', example: 'number'})
+  @ApiProperty({description: 'Количество', example: 'decimal(13,3)'})
   @Column({type: DataType.DECIMAL(13,3)})
   nsu_menge: DecimalDataType;
-  @ApiProperty({description: 'Ставка НДС', example: 'string(2)'})
+  @ApiProperty({description: 'Ставка НДС', example: 'varchar(2)'})
   @Column({type: DataType.STRING(2)})
   vat_rate: string;
-  @ApiProperty({description: 'Наименование расценки', example: 'string(1333)'})
+  @ApiProperty({description: 'Наименование расценки', example: 'varchar(1333)'})
   @Column({type: DataType.STRING(1333)})
   alt_name_unit: string;
-  @ApiProperty({description: 'Комментарий к НДС в КП', example: 'string(1333)'})
+  @ApiProperty({description: 'Комментарий к НДС в КП', example: 'varchar(1333)'})
   @Column({type: DataType.STRING(1333)})
   nds_comm: string;
 }
