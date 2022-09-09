09b0bb4d9ffeac7aab9689106f5b22bfcb0102e2
asm1th <asm1th@yandex.ru>
2022-09-09 12:12:35 +0300

    fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { JWTAuthGuard } from './auth/jwt-auth.guard';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000
-  const app = await NestFactory.create(AppModule, {cors: false});
+  const app = await NestFactory.create(AppModule, {cors: true});
   //const app = await NestFactory.create(AppModule);
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
