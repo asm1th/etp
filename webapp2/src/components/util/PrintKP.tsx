5fc6fbfe36c2c7a77e52f77d86631bba86341a41
Alex <klealekle@gmail.com>
2022-09-09 12:13:42 +0300

    key

--- webapp2/src/components/util/PrintKP.tsx
+++ webapp2/src/components/util/PrintKP.tsx
@@ -73,7 +73,7 @@ const PrintKP: FC = () => {
 
 
                 {stags.map(({ kp_stage_guid, opr_usl_stage, opr_usl_stage_num, stagSumm, stagSumm_nds, isNoNds }) => (
-                    <>
+                    <div key="opr_usl_stage_num">
                         <Layout className="kpHeader">
                             <Text>
                                 Этап {opr_usl_stage_num} {opr_usl_stage}
@@ -140,7 +140,7 @@ const PrintKP: FC = () => {
                                 </Layout>
                             </Layout>
                         </div>
-                    </>
+                    </div>
                 ))}
 
                 <Layout className="TravelRow">
