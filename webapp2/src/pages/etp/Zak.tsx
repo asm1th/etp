13f83e48d7fc7721244d4ac103700f754abc662b
Alex <klealekle@gmail.com>
2022-09-15 15:38:12 +0300

    zak фильтр

--- webapp2/src/pages/etp/Zak.tsx
+++ webapp2/src/pages/etp/Zak.tsx
@@ -4,14 +4,25 @@ import { useAppDispatch, useAppSelector } from "../../hooks/redux";
 import { dashSlice } from "../../store/reducers/dash/dashSlice";
 import Sidebar from "../../components/dash/Sidebar";
 import ZakFilterProc from '../../components/zak/ZakFilterProc';
+import { Breadcrumbs } from '@consta/uikit/BreadcrumbsCanary';
+import './Zak.css'
 
 const Zak: FC = () => {
     const dispatch = useAppDispatch()
 
-    const {isToggleSidebar} = useAppSelector(state => state.dashReducer)
+    const { isToggleSidebar } = useAppSelector(state => state.dashReducer)
     const handleToggleSidebar = (checked: boolean) => {
         dispatch(dashSlice.actions.setToggleSidebar())
     };
+    //Закупочные процедуры
+
+    const pagesNoIcon = [{
+        label: 'Главная',
+        href: '/',
+    }, {
+        label: 'Закупочные процедуры',
+        href: '/etp/zak',
+    }]
 
     return (
         <>
@@ -22,10 +33,10 @@ const Zak: FC = () => {
             />
             <main>
                 <DashHeader2 />
-                
                 <ZakFilterProc />
-
-                
+                <div className="zakContainer">
+                    
+                </div>
             </main>
         </>
     );
