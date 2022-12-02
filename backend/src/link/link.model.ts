f66b0896a126063778fd0f9e2c3b6fa37e3d074c
Kostya2702 <loshkarev.kostya27@gmail.com>
2022-12-02 17:08:58 +0300

    Updated
    - Added new fields
    - Changed data type for some fields

--- backend/src/link/link.model.ts
+++ backend/src/link/link.model.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { Column, DataType, Model, Table, HasMany } from "sequelize-typescript";
-import { DateOnlyDataType, DecimalDataType} from "sequelize/types";
-import { Samp } from "../samp/samp.model";
+import { Column, DataType, Model, Table, HasMany, ForeignKey } from "sequelize-typescript";
+import { DateOnlyDataType, DecimalDataType, UUIDV4 } from "sequelize/types";
+import { Route } from "../route/route.model";
 
 interface KpLinkAttrs{
   link: string;
@@ -17,24 +17,42 @@ interface KpLinkAttrs{
 export class KpLink extends Model<KpLink, KpLinkAttrs> {
   @ApiProperty({description: 'Ключ для расценки контрагента в шаблоне КП', example: 'string(32)'})
   @Column({type: DataType.STRING(32), unique: true, primaryKey: true})
+  @ForeignKey( () => Route)
   link: string;
+
+  @ApiProperty({description: '', example: 'uuid'})
+  @Column({type: DataType.UUIDV4()})
+  kp_sample_guid: typeof UUIDV4;
+
   @ApiProperty({description: 'Email', example: 'string(128)'})
   @Column({type: DataType.STRING(128)})
   info_ka_email: string;
+
   @ApiProperty({description: 'Наименование Участника(Компании)', example: 'string(132)'})
   @Column({type: DataType.STRING(132)})
   info_ka_name: string;
+
+  @ApiProperty({description: 'ИНН', example: 'string(16)'})
+  @Column({type: DataType.STRING(16)})
+  inn: string;
+
+  @ApiProperty({description: 'КПП', example: 'string(18)'})
+  @Column({type: DataType.STRING(18)})
+  kpp: string;
+
   @ApiProperty({description: 'Срок действия предложения от контрагента', example: 'date(YYYYMMDD)'})
   @Column({type: DataType.DATEONLY})
   kp_offer_expire_date: DateOnlyDataType;
+  
   @ApiProperty({description: 'Командировочные расходы', example: 'number'})
   @Column({type: DataType.DECIMAL(17,2)})
   travel_exp: DecimalDataType;
+
   @ApiProperty({description: 'Комментарий к командировочным расходам', example: 'string(1333)'})
   @Column({type: DataType.STRING(1333)})
   travel_exp_comm: string;
 
-  @HasMany( () => Samp) 
-  samps: Samp[];  
-  
+  @ApiProperty({description: '', example: 'boolean'})
+  @Column({type: DataType.BOOLEAN})
+  fl_del: boolean;
 }
