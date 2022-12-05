518e01c6462a558619493b6034cc180aa10413d8
Kostya2702 <loshkarev.kostya27@gmail.com>
2022-12-05 16:20:31 +0300

    Added module cost
    - Added cost model
    - Created default files for controller and unit testing

---
+++ backend/src/cost/cost.model.ts
@@ -0,0 +1,83 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { AllowNull, BelongsTo, Column, DataType, ForeignKey, Model, Table } from "sequelize-typescript";
+import { Route } from "src/route/route.model";
+import { Stag } from "src/stag/stag.model";
+
+interface ValueAttrs {
+  kp_cost_guid: string;
+  kp_stage_guid: string;
+  cost_type: string;
+  cost_name: string;
+  cost_meins: string;
+  cost_menge: string;
+  cost_month: string;
+  cost_months_use: string;
+  cost_months_useful: string;
+  cost_price: string;
+  cost_per_month: string;
+}
+
+@Table({tableName:'ztin_suz_kp_route', createdAt: false, updatedAt: false})
+export class Value extends Model<Value, ValueAttrs> {
+  @ApiProperty({description: 'Ключ затраты', example: 'uuid'})
+  @AllowNull(false)
+  @ForeignKey(() => Route)
+  @Column({type: DataType.UUIDV4, primaryKey: true})
+  kp_cost_guid: any;
+
+  @ApiProperty({description: '', example: 'uuid'})
+  @ForeignKey(() => Stag)
+  @Column({type: DataType.UUIDV4, primaryKey: true})
+  kp_stage_guid: any;
+
+  @ApiProperty({description: 'Наименование', example: 'varchar(2)'})
+  @AllowNull(false)
+  @Column({type: DataType.STRING(2)})
+  cost_type: string;
+
+  @ApiProperty({description: '', example: 'varchar(2)'})
+  @AllowNull(false)
+  @Column({type: DataType.STRING(2)})
+  cost_name: string;
+
+  @ApiProperty({description: '', example: 'varchar(3)'})
+  @AllowNull(false)
+  @Column({type: DataType.STRING(3)})
+  cost_meins: string;
+
+  @ApiProperty({description: 'Количество ЕИ', example: 'decimal(13, 3)'})
+  @AllowNull(false)
+  @Column({type: DataType.DECIMAL(13, 3)})
+  cost_menge: any;
+
+  @ApiProperty({description: 'Количество месяцев', example: 'number'})
+  @AllowNull(false)
+  @Column({type: DataType.NUMBER})
+  cost_month: number;
+
+  @ApiProperty({description: 'Кол-во месяцев использования', example: 'number'})
+  @AllowNull(false)
+  @Column({type: DataType.NUMBER})
+  cost_months_use: number;
+
+  @ApiProperty({description: 'Срок полезного использования', example: 'number'})
+  @AllowNull(false)
+  @Column({type: DataType.NUMBER})
+  cost_months_useful: number;
+
+  @ApiProperty({description: 'Цена', example: 'decimal(17, 2)'})
+  @AllowNull(false)
+  @Column({type: DataType.DECIMAL(17, 2)})
+  cost_price: number;
+
+  @ApiProperty({description: 'Кол-во чел/мес', example: 'number'})
+  @AllowNull(false)
+  @Column({type: DataType.NUMBER})
+  cost_per_month: number;
+
+  @BelongsTo(() => Stag)
+  stag: Stag;
+
+  @BelongsTo(() => Route)
+  route: Route;
+}
