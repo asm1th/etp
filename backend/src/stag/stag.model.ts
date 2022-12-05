3f222f4c77656377e342eb3098d6401d8971e263
Kostya2702 <loshkarev.kostya27@gmail.com>
2022-12-05 15:36:40 +0300

    Updated to new version

--- backend/src/stag/stag.model.ts
+++ backend/src/stag/stag.model.ts
@@ -9,7 +9,8 @@ interface StagAttrs{
   kp_sample_guid: string;
   opr_usl_stage_id: string;
   opr_usl_stage: string;
-  opr_usl_stage_num: SmallIntegerDataType;
+  opr_usl_stage_num: number;
+  fl_del: boolean;
 }
 
 @Table({tableName:'ztin_suz_kp_stag', createdAt: false, updatedAt: false})
@@ -17,19 +18,27 @@ export class Stag extends Model<Stag, StagAttrs> {
   @ApiProperty({description: 'Ключ шаблона КП', example: 'varchar(32)'})
   @Column({type: DataType.STRING(32), unique: true, primaryKey: true})
   kp_stage_guid: string;
+
   @ApiProperty({description: 'Ключ шаблона КП', example: 'varchar(32)'})
   @ForeignKey( () => Samp) 
   @Column({type: DataType.STRING(32), unique: false})
   kp_sample_guid: string;
+
   @ApiProperty({description: 'ID этапа', example: 'varchar(12)'})
   @Column({type: DataType.STRING(12)})
   opr_usl_stage_id: string;
+
   @ApiProperty({description: 'Наименование этапа', example: 'varchar(1333)'})
   @Column({type: DataType.STRING(1333)})
   opr_usl_stage: string;
-  @ApiProperty({description: 'Наименование этапа', example: 'smallInt'})
-  @Column({type: DataType.SMALLINT})
-  opr_usl_stage_num: SmallIntegerDataType; 
+
+  @ApiProperty({description: 'Идентификатор этапа', example: 'smallInt'})
+  @Column({type: DataType.SMALLINT()})
+  opr_usl_stage_num: number; 
+
+  @ApiProperty({description: '', example: 'boolean'})
+  @Column({type: DataType.BOOLEAN})
+  fl_del: boolean;
 
   @HasMany( () => Unit) 
   units: Unit[];
--- backend/src/unit/unit.model.ts
+++ backend/src/unit/unit.model.ts
@@ -1,8 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { BelongsTo, Column, DataType, ForeignKey, HasMany, HasOne, Model, Table } from "sequelize-typescript";
-import { DecimalDataType} from "sequelize/types";
-import { Usrp } from "../usrp/usrp.model";
+import { BelongsTo, Column, DataType, ForeignKey, HasMany, Model, Table } from "sequelize-typescript";
 import { Stag } from "../stag/stag.model";
+import { Route } from "../route/route.model";
 
 interface UnitAttrs{
   kp_unit_guid: string;
@@ -10,43 +9,60 @@ interface UnitAttrs{
   opr_usl_unit_id: string;
   usl_quan_unit: string;
   opr_usl_unit: string;
-  nsu_menge: DecimalDataType;
+  nsu_menge: number;
   vat_rate: string;
-  opr_usl_unit_restr_quan: string;
-  opr_usl_unit_restr_menge: string;
+  opr_usl_unit_restr_quan: boolean;
+  opr_usl_unit_restr_menge: boolean;
+  fl_del: boolean;
 }
 
 @Table({tableName:'ztin_suz_kp_unit', createdAt: false, updatedAt: false})
 export class Unit extends Model<Unit, UnitAttrs> {
-  @ApiProperty({description: 'Ключ расченки в шаблоне КП', example: 'varchar(32)'})
+  @ApiProperty({description: 'Ключ расценки в шаблоне КП', example: 'varchar(32)'})
+  @ForeignKey( () => Route)
   @Column({type: DataType.STRING(32), unique: true, primaryKey: true})
   kp_unit_guid: string;
+
   @ApiProperty({description: 'Ключ шаблона КП', example: 'varchar(32)'})
   @ForeignKey( () => Stag)
   @Column({type: DataType.STRING(32)})
   kp_stage_guid: string;
+
   @ApiProperty({description: 'ID Расценки', example: 'varchar(12)'})
   @Column({type: DataType.STRING(12)})
   opr_usl_unit_id: string;
+
   @ApiProperty({description: 'Единица измерения', example: 'varchar(3)'})
   @Column({type: DataType.STRING(3)})
   usl_quan_unit: string;
+
   @ApiProperty({description: 'Наименование расценки', example: 'varchar(1333)'})
   @Column({type: DataType.STRING(1333)})
   opr_usl_unit: string;
+
   @ApiProperty({description: 'Количество', example: 'decimal(13,3)'})
   @Column({type: DataType.DECIMAL(13,3)})
-  nsu_menge: DecimalDataType;
+  nsu_menge: number;
+
   @ApiProperty({description: 'Ставка НДС', example: 'varchar(2)'})
   @Column({type: DataType.STRING(2)})
   vat_rate: string;
-  @ApiProperty({description: 'Ограничить ЕИ', example: 'varchar(1)'})
-  @Column({type: DataType.STRING(1)})
-  opr_usl_unit_restr_quan: string;
-  @ApiProperty({description: 'Ограничить количество ЕИ', example: 'varchar(1)'})
-  @Column({type: DataType.STRING(1)})
-  opr_usl_unit_restr_menge: string;
 
-  @HasMany( () => Usrp) 
-  usrps: Usrp[];
+  @ApiProperty({description: 'Ограничить ЕИ', example: 'boolean'})
+  @Column({type: DataType.BOOLEAN})
+  opr_usl_unit_restr_quan: boolean;
+
+  @ApiProperty({description: 'Ограничить количество ЕИ', example: 'boolean'})
+  @Column({type: DataType.BOOLEAN})
+  opr_usl_unit_restr_menge: boolean;
+
+  @ApiProperty({description: '', example: 'boolean'})
+  @Column({type: DataType.BOOLEAN})
+  fl_del: boolean;
+
+  @BelongsTo( () => Route) 
+  route: Route;
+
+  @HasMany( () => Stag) 
+  stags: Stag[];
 }
