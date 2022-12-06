46f7cd4039db79059895cebb87637fa66116f608
Kostya2702 <loshkarev.kostya27@gmail.com>
2022-12-06 18:13:55 +0300

    Updated data type for fields

--- backend/src/link/dto/update_link.dto.ts
+++ backend/src/link/dto/update_link.dto.ts
@@ -1,13 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { DateOnlyDataType, DecimalDataType } from "sequelize/types";
 
 export class UpdateLinkDTO{
   @ApiProperty({description: 'Ключ для расценки контрагента в шаблоне КП', example: 'string'})
   readonly link: string;
   @ApiProperty({description: 'Срок действия предложения от контрагента', example: 'date(YYYYMMDD)'})
-  readonly kp_offer_expire_date: DateOnlyDataType;
+  readonly kp_offer_expire_date: Date;
   @ApiProperty({description: 'Командировочные расходы', example: 'number'})
-  travel_exp: DecimalDataType;
+  travel_exp: number;
   @ApiProperty({description: 'Комментарий к командировочным расходам', example: 'string'})
   readonly travel_exp_comm: string;
 }
