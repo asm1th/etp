c12b83cee1d6b53686bb9c4355ce39d34c840453
Kostya2702 <loshkarev.kostya27@gmail.com>
2022-12-06 18:11:34 +0300

    Updated ralationshpis and data type for fields

--- backend/src/samp/samp.model.ts
+++ backend/src/samp/samp.model.ts
@@ -1,26 +1,32 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { BelongsTo, Column, DataType, Default, ForeignKey, Model, Table } from "sequelize-typescript";
+import { BelongsTo, Column, DataType, Default, ForeignKey, HasMany, Model, Table } from "sequelize-typescript";
+import { Stag } from "src/stag/stag.model";
 import { Route } from "../route/route.model";
 
 interface SampAttrs{
-  kp_sample_guid: string;
+  kp_sample_guid: any;
   konkurs_id: string;
   lot_id: string;
-  kp_accep_date: Date;
   kp_send_date: Date;
+  kp_send_time: Date;
+  kp_author: string;
+  kp_crt_date: Date;
+  kp_crt_time: Date;
+  kp_contr_end_date: Date;
+  sample_type: string;
+  opr_usl_segment_id: string;
+  opr_usl_subseg_id: string;
+  opr_usl_code: string;
+  fl_del: boolean;
 }
 
 @Table({tableName:'ztin_suz_kp_samp', createdAt: false, updatedAt: false})
 export class Samp extends Model<Samp, SampAttrs> {
   @ApiProperty({description: 'Ключ шаблона КП', example: 'uuid'})
-  @Column({type: DataType.UUIDV4, unique: true, primaryKey: true})
   @Default(DataType.UUIDV4)
   @ForeignKey(() => Route)
+  @Column({type: DataType.UUIDV4(), unique: true, primaryKey: true})
   kp_sample_guid: any;
-
-  // Relation to entity Route
-  @BelongsTo(() => Route)
-  kp_sample: Route;
   
   @ApiProperty({description: 'Идентификатор конкурса', example: 'varchar(12)'})
   @Column({type: DataType.STRING(12)})
@@ -77,4 +83,10 @@ export class Samp extends Model<Samp, SampAttrs> {
   @ApiProperty({description: '', example: 'boolean'})
   @Column({type: DataType.BOOLEAN()})
   fl_del: boolean;
+
+  @BelongsTo(() => Route)
+  routes: Route;
+
+  @HasMany(() => Stag)
+  stages: Stag;
 }
