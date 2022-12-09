d8998c132619de3843fc225cccc340ea5b4590a1
Kostya2702 <loshkarev.kostya27@gmail.com>
2022-12-09 17:20:43 +0300

    Changed updateUsrp service
    Added method set() for updating entry in database
    Fix updating multiply entries!

--- backend/src/usrp/usrp.service.ts
+++ backend/src/usrp/usrp.service.ts
@@ -17,20 +17,19 @@ export class UsrpService {
   }
 
   async updateUsrp(dto: UpdateUsrpDTO){
-    const usrp = await this.userRepository.findByPk(dto.kp_unit_guid);
-    if (!usrp) {
+    let usrp: Usrp;
+
+    try {
+      usrp = await this.userRepository.findOne({
+        where: {
+          kp_usrp_guid: dto.kp_usrp_guid
+        }
+      });
+      usrp.set(dto);
+      return await usrp.save();
+    } catch(err) {
+      console.error(err)
       throw new HttpException('Ключ расценки в шаблоне КП не найден', HttpStatus.NOT_FOUND);  
     }
-    usrp.kp_unit_guid = dto.kp_unit_guid;
-    usrp.prices_user = dto.prices_user;
-    usrp.usl_quan_unit = dto.usl_quan_unit;
-    usrp.nsu_menge = dto.nsu_menge;
-    usrp.vat_rate = dto.vat_rate;
-    usrp.alt_name_unit = dto.alt_name_unit;
-    usrp.nds_comm = dto.nds_comm;
-    await usrp.save();
-    return usrp;
-
-    
   }
 }
