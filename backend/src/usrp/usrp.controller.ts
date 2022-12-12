d55dc8a92af33a2cee1e96299e105584f26d3683
Kostya2702 <loshkarev.kostya27@gmail.com>
2022-12-12 16:20:55 +0300

    Bug fixes
    - Added async/await for functions

--- backend/src/usrp/usrp.controller.ts
+++ backend/src/usrp/usrp.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpStatus, Param, Put } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, ParseUUIDPipe, Put } from '@nestjs/common';
 import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UpdateUsrpDTO } from './dto/update_usrp.dto';
 import { Usrp } from './usrp.model';
@@ -12,22 +12,22 @@ export class UsrpController {
   @ApiOperation({summary: 'Получение всех цен от контрагентов'})
   @ApiResponse({ status: HttpStatus.OK, description: "Success", type: Usrp})
   @Get()
-  getAll(){
-    return this.UsrpService.getAllUsrps();
+  async getAll() {
+    return await this.UsrpService.getAllUsrps();
   }
 
   @ApiOperation({summary: 'Получение цены от контрагентов'})
   @ApiParam({ name: "link", required: true, description: "Ключ для расценки контрагента в шаблоне КП" })
   @ApiResponse({ status: HttpStatus.OK, description: "Success", type: Usrp })
   @Get(':kp_unit_guid')
-  getEntity(@Param('kp_unit_guid') kp_unit_guid: string){
-    return this.UsrpService.getOneUsrp(kp_unit_guid)
+  async getEntity(@Param('kp_unit_guid', new ParseUUIDPipe()) kp_unit_guid: string) {
+    return await this.UsrpService.getOneUsrp(kp_unit_guid)
   }
 
   @ApiOperation({summary: 'Обновление данных по командировке в шаблоне КП'})
   @ApiResponse({ status: HttpStatus.OK, description: "Success", type: UpdateUsrpDTO })
   @Put()
-  async modifyEntity(@Body() dto: UpdateUsrpDTO): Promise<any>{
-    return this.UsrpService.updateUsrp(dto)
+  async modifyEntity(@Body() dto: UpdateUsrpDTO): Promise<Usrp> {
+    return await this.UsrpService.updateUsrp(dto)
   }
 }
