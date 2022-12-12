44aebb3c6226c599f47541b1688571308c53f4dd
Kostya2702 <loshkarev.kostya27@gmail.com>
2022-12-12 19:05:39 +0300

    Added loading model Usrp with attributes

--- backend/src/unit/unit.service.ts
+++ backend/src/unit/unit.service.ts
@@ -19,41 +19,12 @@ export class UnitService {
   }
 
   async getUnitWithUsrp(kp_unit_guid: string) {
-    const unit = await this.userRepository.findOne({
+    return await this.userRepository.findOne({
       where: {kp_unit_guid}, 
-      include: Usrp
+      include: [{
+        model: Usrp,
+        attributes: ['link_id', 'prices_user', 'alt_name_unit', 'nds_comm']
+      }]
     });
-    
-    // let usrps: object[];
-    // unit.usrps.forEach( usrp => {
-    //   usrps.push({
-    //     'link_id': usrp.link_id,
-    //     'prices_user': usrp.prices_user,
-    //     'alt_name_unit': usrp.alt_name_unit,
-    //     'nds_comm': usrp.nds_comm
-    //   })
-    // });
-    // // const linkIndex = usrps.findIndex( usrps => usrps.kp_unit_guid === kp_unit_guid );
-    // // const usrp = usrps[linkIndex];
-
-    // const resultDTO = {
-    //   kp_unit_guid:             unit.kp_unit_guid,
-    //   // link_id:                  usrp.link_id,
-    //   kp_stage_guid:            unit.kp_stage_guid,
-    //   opr_usl_unit_id:          unit.opr_usl_unit_id,
-    //   usl_quan_unit:            unit.usl_quan_unit,
-    //   opr_usl_unit:             unit.opr_usl_unit,
-    //   nsu_menge:                unit.nsu_menge,
-    //   vat_rate:                 unit.vat_rate,
-    //   opr_usl_unit_restr_quan:  unit.opr_usl_unit_restr_quan,
-    //   opr_usl_unit_restr_menge: unit.opr_usl_unit_restr_menge,
-    //   // prices_user:              usrp.prices_user, 
-    //   // alt_name_unit:            usrp.alt_name_unit,
-    //   // nds_comm:                 usrp.nds_comm,
-    //   // usrps:                    usrps
-    // };
-
-    // return resultDTO;
-    return unit;
   }
 }
