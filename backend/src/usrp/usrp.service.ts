6595d8ce582ce03a357dd5796fdb7ef41e474eb3
Kostya2702 <loshkarev.kostya27@gmail.com>
2022-12-14 12:38:14 +0300

    Updated keys for finding entries, and changed method for update entity Usrp

--- backend/src/usrp/usrp.service.ts
+++ backend/src/usrp/usrp.service.ts
@@ -7,29 +7,23 @@ import { Usrp } from './usrp.model';
 export class UsrpService {
   constructor(@InjectModel(Usrp) private userRepository: typeof Usrp) { }
 
-  async getAllUsrps() {
-    const Usrps = await this.userRepository.findAll();
-    return Usrps;
+  async getAllUsrps(): Promise<Usrp[]> {
+    return await this.userRepository.findAll();
   }
 
-  async getOneUsrp(kp_unit_guid: string){
-    return this.userRepository.findOne({where: {kp_unit_guid}, include: { all: true, nested: true }});
+  async getOneUsrp(kp_usrp_guid: string): Promise<Usrp> {
+    return await this.userRepository.findOne({
+      where: {kp_usrp_guid}, 
+      include: { all: true, nested: true }
+    });
   }
 
-  async updateUsrp(dto: UpdateUsrpDTO){
-    let usrp: Usrp;
-
-    try {
-      usrp = await this.userRepository.findOne({
-        where: {
-          kp_usrp_guid: dto.kp_usrp_guid
-        }
-      });
-      usrp.set(dto);
-      return await usrp.save();
-    } catch(err) {
-      console.error(err)
-      throw new HttpException('Ключ расценки в шаблоне КП не найден', HttpStatus.NOT_FOUND);  
-    }
+  async updateUsrp(kp_usrp_guid: string, dto: UpdateUsrpDTO) {
+    return await this.userRepository.update(
+      {... dto},
+      {where: {
+        kp_usrp_guid: kp_usrp_guid
+      }}
+    );
   }
 }
