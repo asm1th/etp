a06c45d82a4a3f0534bb1ac07892e0c534cf82f3
Kostya2702 <loshkarev.kostya27@gmail.com>
2022-12-14 18:28:28 +0300

    Added jwt strategy for checking user auth

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports:[
     forwardRef( () => UsersModule),
     JwtModule.register({
