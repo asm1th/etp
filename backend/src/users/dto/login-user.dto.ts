f4ed638c87ebeae230b675f098e1b5d1bd153466
Kostya2702 <loshkarev.kostya27@gmail.com>
2022-12-14 18:08:21 +0300

    Added validators and field lastname for create-user dto

--- backend/src/users/dto/login-user.dto.ts
+++ backend/src/users/dto/login-user.dto.ts
@@ -1,8 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class LoginUserDto{
   @ApiProperty({description: 'Email'})
+  @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
+
   @ApiProperty({description: 'Пароль'})
+  @IsNotEmpty()
+  @IsString()
   readonly password: string;
 }
